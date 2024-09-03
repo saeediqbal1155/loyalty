9f5821143edefef7730ce721bbaa41467b8f3048
Saeed Iqbal <xp.saeed.iqbal@inovalon.com>
2024-09-03 20:13:19 +0500

    Initial Project Creation

--- backend/src/main/java/com/retail/loyalty/repository/CampaignRepository.java
+++
@@ -1,9 +0,0 @@
-package com.retail.loyalty.repository;
-
-import com.retail.loyalty.entity.Campaign;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
-
-public interface CampaignRepository extends ReactiveCrudRepository<Campaign, Long> {
-    Flux<Campaign> findByActiveTrue();
-}
