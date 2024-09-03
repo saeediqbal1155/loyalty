e251b7a206974cf4c495f6723a9ac27b31935b6c
Saeed Iqbal <xp.saeed.iqbal@inovalon.com>
2024-09-03 20:13:58 +0500

    Initial Project Creation

---
+++ target/generated-sources/annotations/com/retail/loyalty/entity/mapper/CampaignMapperImpl.java
@@ -0,0 +1,47 @@
+package com.retail.loyalty.entity.mapper;
+
+import com.retail.loyalty.entity.Campaign;
+import com.retail.loyalty.entity.dto.CampaignDTO;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-09-02T12:12:01+0500",
+    comments = "version: 1.6.0, compiler: javac, environment: Java 21.0.3 (Oracle Corporation)"
+)
+public class CampaignMapperImpl implements CampaignMapper {
+
+    @Override
+    public Campaign toEntity(CampaignDTO campaignDTO) {
+        if ( campaignDTO == null ) {
+            return null;
+        }
+
+        Campaign campaign = new Campaign();
+
+        campaign.setId( campaignDTO.getId() );
+        campaign.setName( campaignDTO.getName() );
+        campaign.setActive( campaignDTO.isActive() );
+        campaign.setStartDate( campaignDTO.getStartDate() );
+        campaign.setEndDate( campaignDTO.getEndDate() );
+
+        return campaign;
+    }
+
+    @Override
+    public CampaignDTO toDTO(Campaign purchase) {
+        if ( purchase == null ) {
+            return null;
+        }
+
+        CampaignDTO campaignDTO = new CampaignDTO();
+
+        campaignDTO.setId( purchase.getId() );
+        campaignDTO.setName( purchase.getName() );
+        campaignDTO.setActive( purchase.isActive() );
+        campaignDTO.setStartDate( purchase.getStartDate() );
+        campaignDTO.setEndDate( purchase.getEndDate() );
+
+        return campaignDTO;
+    }
+}
---
+++ target/generated-sources/annotations/com/retail/loyalty/entity/mapper/RewardMapperImpl.java
@@ -0,0 +1,41 @@
+package com.retail.loyalty.entity.mapper;
+
+import com.retail.loyalty.entity.Reward;
+import com.retail.loyalty.entity.dto.RewardDTO;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-09-02T12:12:01+0500",
+    comments = "version: 1.6.0, compiler: javac, environment: Java 21.0.3 (Oracle Corporation)"
+)
+public class RewardMapperImpl implements RewardMapper {
+
+    @Override
+    public Reward toEntity(RewardDTO rewardDTO) {
+        if ( rewardDTO == null ) {
+            return null;
+        }
+
+        Reward reward = new Reward();
+
+        reward.setName( rewardDTO.getName() );
+        reward.setPointsRequired( rewardDTO.getPointsRequired() );
+
+        return reward;
+    }
+
+    @Override
+    public RewardDTO toDTO(Reward purchase) {
+        if ( purchase == null ) {
+            return null;
+        }
+
+        RewardDTO rewardDTO = new RewardDTO();
+
+        rewardDTO.setName( purchase.getName() );
+        rewardDTO.setPointsRequired( purchase.getPointsRequired() );
+
+        return rewardDTO;
+    }
+}
